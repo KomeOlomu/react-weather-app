4065037874a0bc097c05c4ccc48843bd65ddd933
Uyoyoghene <hopewadells@gmail.com>
2023-06-21 16:54:14 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Kome Olomu and is
-        <a
-          href="https://github.com/KomeOlomu/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Kome Olomu and is{" "}
+          <a
+            href="https://github.com/KomeOlomu/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
