7e10c34f437121ba50a75d52954f30dca1aff422
Uyoyoghene <hopewadells@gmail.com>
2023-06-21 16:15:19 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Kome Olomu and is
+        <a
+          href="https://github.com/KomeOlomu/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
